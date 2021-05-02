161c88f49aa5c2a7cc9d0f137ff197855887eb4f
Merrin <merrin@Merrins-MacBook-Pro.local>
2021-05-02 11:21:22 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='shopee',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='merrin',
+    author_email='merrin.kurian@gmail.com',
+    description='Shopee Kaggle project'
+)
