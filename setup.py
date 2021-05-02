a7d68d0f3b01183f7c9ef66a84c27fb478ed6a54
Merrin <merrin@Merrins-MacBook-Pro.local>
2021-05-02 11:26:07 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import setup
 
+requirements = [
+    "pandas",
+    "matplotlib",
+    "seaborn",
+    "tqdm",
+    "imagehash",
+    "scikit-image",
+    "Pillow",
+    "opencv-python",
+    "torch",
+    "torchvision",
+    "pytorch-lightning"
+]
+
 setup(
     name='shopee',
     version='1.0',
@@ -7,6 +21,7 @@ setup(
     url='',
     license='',
     author='merrin',
-    author_email='merrin.kurian@gmail.com',
-    description='Shopee Kaggle project'
+    author_email='',
+    description='Shopee-Kaggle',
+    install_requires=requirements
 )
