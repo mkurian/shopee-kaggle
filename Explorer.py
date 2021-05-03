ccc2bacb7d525cffb4d8f89859d6a5f3d9d467ce
Merrin <merrin@Merrins-MacBook-Pro.local>
2021-05-02 18:31:32 -0700

    Generate pairs of matching and non-matching images using a subset if 1000 images from training set

--- Explorer.py
+++ Explorer.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 import tqdm
 from tqdm.auto import tqdm as tqdmp
 
@@ -23,16 +23,39 @@ warnings.simplefilter("ignore")
 
 class DataLoader:
     def __init__(self, path):
-        self.path = pd.read_csv(path)
+        self.path = path
 
-    def load_train_df(self, csvpath, imagepath):
-        self.train_csvpath = ('/').join(self.path, csvpath)
-        self.train_df = pd.read_csv(self.train_csvpath)
-        return self.train_df
+    def load_df(self, csvpath, imagepath):
+        self.csvpath = ('/').join([self.path, csvpath])
+        self.df = pd.read_csv(self.csvpath)
+        return self.df
+
+    def generate_image_path(self):
+        self.train_df = self.load_df('train.csv', 'train_images')
+        self.test_df = self.load_df('test.csv', 'test_images')
+
+        print(f"train: {self.train_df.shape}  test: {self.test_df.shape}")
+        print(f"unique labels: {self.train_df.label_group.nunique()}")
+
+        self.dataLoader.preprocess_train(self.train_df)
+        self.dataLoader.preprocess_test(self.test_df)
+
+    def preprocess_train(self, df):
+        df['path'] = self.path + '/train_images/' + df['image']
+        df.to_csv(self.path + '/train_proc.csv')
+
+    def preprocess_test(self, df):
+        df['path'] = self.path + '/test_images/' + df['image']
+        df.to_csv(self.path + '/test_proc.csv')
+
 
 class ImageHandler:
 
     # def __init__(self):
+
+    def image_shape(self, image_path):
+        im = cv2.imread(image_path)
+        return str(im.shape)
 
     def image_viz(self, image_path):
         img = cv2.imread(image_path)
@@ -51,3 +74,64 @@ class ImageHandler:
             plt.subplot(1, 2, idx + 1)
             self.image_viz(path)
         plt.show()
+
+
+class MatchFinder:
+    def match_matrix(self, phash_array):
+        """ A function that checks for matches by phash value,
+        takes phash values as input, outputs diff matrix as a df """
+        phashs = phash_array.apply(lambda x: imagehash.hex_to_hash(x))
+        phash_matrix = pd.DataFrame()
+        for idx, i in enumerate(phash_array):
+            phash_matrix = pd.concat([phash_matrix, phashs - imagehash.hex_to_hash(i)], axis=1)
+        phash_matrix.columns = range(len(phash_array))
+        return phash_matrix
+
+
+class GeneratePairs:
+
+    def generate_matching_pairs(self, matches):
+        matchingsets = []
+        for i in range(len(matches)):
+            matchingsets.append(matches.iloc[i, :][matches.iloc[i, :] == 0].index.values)
+        matchingsets = pd.Series(matchingsets)
+        pairs = matchingsets[matchingsets.apply(lambda x: len(x) == 2)]
+        triplets = matchingsets[matchingsets.apply(lambda x: len(x) == 3)]
+        quartets = matchingsets[matchingsets.apply(lambda x: len(x) == 4)]
+
+        matching_pairs = []
+        for idx, val in pairs.items():
+            matching_pairs.append(val)
+
+        for idx, value in triplets.items():
+            matching_pairs.append([value[0], value[1]])
+            matching_pairs.append([value[1], value[2]])
+            matching_pairs.append([value[0], value[2]])
+
+        for idx, value in quartets.items():
+            matching_pairs.append([value[0], value[1]])
+            matching_pairs.append([value[1], value[2]])
+            matching_pairs.append([value[0], value[2]])
+            matching_pairs.append([value[0], value[3]])
+            matching_pairs.append([value[1], value[3]])
+            matching_pairs.append([value[2], value[3]])
+
+        final = list(set([tuple(t) for t in matching_pairs]))
+
+        return pd.DataFrame().from_records(final, columns=['one', 'two'])
+
+    def generate_nonmatching_pairs(self, matching_pairs):
+        non_matching_pairs = []
+        for i in range(len(matching_pairs)):
+            if [i, i+1] not in matching_pairs:
+                non_matching_pairs.append([i, i+1])
+            if len(non_matching_pairs) == len(matching_pairs):
+                break
+        return pd.DataFrame().from_records(non_matching_pairs, columns=['one', 'two'])
+        return
+
+    def mergeDatasets(self, matching, nonmatching):
+        return
+
+    def saveMergedDataset(self):
+        return
--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import Explorer
 import os
 
 if __name__ == '__main__':
+    imageHandler = Explorer.ImageHandler()
+
     path = '/Users/merrin/Work/Kaggle/shopee-product-matching'
+    dataLoader = Explorer.DataLoader(path)
+    # dataLoader.generate_image_path()
 
-    print(os.listdir(path))
-
-    # dataLoader = Explorer.DataLoader(path)
-    # train_df = dataLoader.load_train_df(path, 'train.csv', 'train_images')
-    #
-    # print(train_df.loc[[11, 12], ['posting_id', 'image_phash', 'title', 'label_group']])
-    # print(train_df.loc[[889, 890, 891], ['posting_id', 'image_phash', 'title', 'label_group']])
-    #
-    # imageHandler = Explorer.ImageHandler()
+    train_df = dataLoader.load_df('train_proc.csv', 'train_images')
+    test_df = dataLoader.load_df('test_proc.csv', 'test_images')
+    print(train_df.shape)
     # imageHandler.plot_image_pairs(train_df, 11, 12)
+    # imageHandler.plot_image(train_df, 11)
+
+    # Since the process of composing a matrix is quite resource-intensive,
+    # for clarity, we will take only the first thousand images.
+    train_1000 = train_df.iloc[:1000, :]
+    train_1000 = train_1000.drop(columns=['Unnamed: 0'])
+    matchFinder = Explorer.MatchFinder()
+    matches = matchFinder.match_matrix(train_1000['image_phash'])
+
+    generator = Explorer.GeneratePairs()
+    matching_pairs = generator.generate_matching_pairs(matches)
+    matching_pairs.to_csv(path + '/matching_pairs_1000.csv')
+    print(f" matching pairs: {len(matching_pairs)}")
+
+    non_matching_pairs = generator.generate_nonmatching_pairs(matching_pairs)
+    non_matching_pairs.to_csv(path + '/non_matching_pairs_1000.csv')
+    print(f" non-matching pairs: {len(non_matching_pairs)}")
